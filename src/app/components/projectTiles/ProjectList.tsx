7389db70a2b7530c5428e91a12669fa5762479d8
quinten.vollmer <quinten.vollmer@db.nl>
2023-07-12 15:56:23 +0200

    Added eslint config and prettier to the project, added a lot of CSS for the Project tiles. Fixed the waiving animation

--- src/app/components/projectTiles/ProjectList.tsx
+++ src/app/components/projectTiles/ProjectList.tsx
@@ -1,56 +1,61 @@
-'use client'
+'use client';
 
-import React, {useState} from 'react';
+import React, { ReactElement, useState } from 'react';
 import clsx from 'clsx';
 
 type Project = {
-    header: string;
-    message: string;
+  header: string;
+  message: string;
+  svg: ReactElement;
 };
 
 type FlipCardProps = {
-    project: Project;
+  project: Project;
 };
 
 type ProjectListProps = {
-    projects: Project[];
+  projects: Project[];
 };
 
-function ProjectList({projects}: ProjectListProps) {
-    return (
-        <div className="flex flex-row items-center content-center justify-center p-6">
-            <h1 className="text-5xl font-bold text-slate-900">Mijn projecten</h1>
-            {projects.map((project, index) => (
-                <FlipCard key={project.header} project={project}/>
-            ))}
-        </div>
-    );
+function ProjectList({ projects }: ProjectListProps) {
+  return (
+    <div className="flex flex-col items-center justify-center p-6 h-[46rem]">
+      <h1 className="text-5xl font-bold text-slate-900">Mijn projecten</h1>
+      <h3 className="text-1xl font-bold text-slate-700 mb-8">
+        Klik op een tegel om meer te weten te komen!
+      </h3>
+      <div className="flex flex-row items-center justify-center">
+        {projects.map((project, index) => (
+          <FlipCard key={project.header} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function FlipCard({ project }: FlipCardProps) {
-    const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    return (
-        <div
-            onClick={() => setIsFlipped(!isFlipped)}
-            className={clsx(
-                "transform transition-transform duration-500 ease-in-out max-w-lg p-6 m-10 border-2 rounded-lg border-slate-500 shadow-2xl shadow-slate-500 bg-slate-200",
-                isFlipped ? "rotate-y-180" : "",
-                "min-w-200 min-h-200"
-            )}
-        >
-            <div className={clsx("min-w-200 min-h-200", isFlipped ? "hidden" : "")}>
-                {/* Insert your SVG here */}
-                <h2 className="text-3xl font-bold text-slate-900"> {project.header} </h2>
-            </div>
-            <div className={clsx("min-w-200 min-h-200", isFlipped ? "" : "hidden")}>
-                <p className="mb-4">
-                    {project.message}
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      onClick={() => setIsFlipped(!isFlipped)}
+      className={clsx(
+        'max-w-lg p-6 m-10 border-2 rounded-lg border-slate-500 shadow-2xl shadow-slate-500 bg-slate-200 cursor-pointer',
+        !isFlipped ? '' : 'animate-rotate-y animate-once animate-delay-200'
+      )}
+    >
+      <div className={clsx('min-w-200 min-h-200', isFlipped ? 'hidden' : '')}>
+        {project.svg}
+      </div>
+      <div className={clsx('min-w-200 min-h-200', isFlipped ? '' : 'hidden')}>
+        <h2 className="text-3xl font-bold text-slate-900">
+          {' '}
+          {project.header}{' '}
+        </h2>
+        <p className="mb-4">{project.message}</p>
+      </div>
+    </div>
+  );
 }
-
 
 export default ProjectList;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}',],
-    theme: {
-        extend: {
-            keyframes: {
-                wave: {
-                    '0%': {transform: 'rotate(0.0deg)'},
-                    '10%': {transform: 'rotate(14deg)'},
-                    '20%': {transform: 'rotate(-8deg)'},
-                    '30%': {transform: 'rotate(14deg)'},
-                    '40%': {transform: 'rotate(-4deg)'},
-                    '50%': {transform: 'rotate(10.0deg)'},
-                    '60%': {transform: 'rotate(0.0deg)'},
-                    '100%': {transform: 'rotate(0.0deg)'},
-                },
-                wiggle: {
-                    '0%, 100%': {transform: 'rotate(-15deg)'},
-                    '50%': {transform: 'rotate(15deg)'},
-                }
-            },
-            animation: {
-                'waving-hand': 'wave 2s linear infinite',
-                wiggle: 'wiggle 1s ease-in-out infinite',
-            },
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
         },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
     },
-    plugins: [
-        require('tailwindcss-animated'),
-    ],
-}
-
+  },
+  plugins: [require('tailwindcss-animated')],
+};
